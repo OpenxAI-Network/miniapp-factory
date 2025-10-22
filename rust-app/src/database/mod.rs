d7faae62090a1a5edcddc211741f550234359314
Plopmenz <plopmenz@gmail.com>
2025-10-22 13:56:51 +0200

    optimize database connection

--- rust-app/src/database/mod.rs
+++ rust-app/src/database/mod.rs
@@ -22,7 +22,9 @@ impl Database {
 
 pub async fn create_connection() -> DatabaseConnection {
     let connection = PgPoolOptions::new()
+        .min_connections(10)
         .max_connections(10000)
+        .test_before_acquire(false)
         .connect(&database())
         .await
         .unwrap_or_else(|e| panic!("Could not establish database connection: {e}"));
